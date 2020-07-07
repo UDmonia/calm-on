bdf15af0207350d57f0bbbb711e87c070b39df4a
Tiffany <chin.tiffany108@gmail.com>
2020-07-06 21:24:49 -0400

    add top box

--- src/components/homescreen.js
+++ src/components/homescreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Button, ImageBackground } from 'react-native';
+import { Text, View, Button, ImageBackground, TouchableOpacity, Image } from 'react-native';
 import styles from '../stylesheets/homescreenStyles';
 
 const Homescreen = props => {
@@ -7,15 +7,39 @@ const Homescreen = props => {
     <View style={styles.format}>
       <ImageBackground source={require('../../assets/splash_panel.png')} style={styles.image}>
         {/* <Text style={styles.text}>Inside</Text> */}
-      <Text>Homescreen</Text>
-
-      <Button
-        title='Home'
-        onPress={() =>
-          props.navigation.navigate('Splash')
-        }
-      />
       
+      <View style={styles.main}>
+        <View style={styles.topBox}>
+
+        </View>
+        <Text>Homescreen</Text>
+
+        <Button
+          title='Home'
+          onPress={() =>
+            props.navigation.navigate('Splash')
+          }
+        />
+      </View>
+
+      <View style={styles.nav}>
+        <View style={styles.navIcons}>
+          <TouchableOpacity>
+              <Image style={styles.homeIcon}
+                source={require('../../assets/icon_home_selected.png')}/>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Image style={styles.achieveIcon}
+              source={require('../../assets/icon_achievements.png')} />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Image style={styles.profileIcon}
+              source={require('../../assets/icon_profile.png')} />
+          </TouchableOpacity>
+        </View>
+      </View>
       </ImageBackground>
 
     </View>
--- src/stylesheets/homescreenStyles.js
+++ src/stylesheets/homescreenStyles.js
@@ -6,11 +6,9 @@ const styles = StyleSheet.create({
     flexDirection: "column"
   },
   image: {
-    // flex: 1,
-    // resizeMode: "cover",
-    // justifyContent: "center",
     width: '100%',
     height: '100%',
+    
   },
   text: {
     color: "grey",
@@ -18,7 +16,50 @@ const styles = StyleSheet.create({
     fontWeight: "bold"
   },
   format: {
-    padding: 0
+    padding: 0,
+  },
+
+  main: {
+    backgroundColor: 'rgba(0, 0, 0, 0.45)',
+    display: 'flex',
+    height: '100%',
+    alignItems: 'center',
+  },
+  topBox: {
+    backgroundColor: '#FFFFFF',
+    borderColor: '#89AAFF',
+    borderRadius: 6,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    height: 108,
+    marginTop: 106,
+    width: 348,
+  },
+  nav: {
+    backgroundColor: '#8AABFF',
+    height: 77,
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+  navIcons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 50,
+  },
+  homeIcon: {
+    height: 36,
+    width: 36,
+  },
+  achieveIcon: {
+    height: 36,
+    width: 41,
+  },
+  profileIcon: {
+    height: 40,
+    width: 40,
   }
 })
 
