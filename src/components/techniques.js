0a28998731ecbe167e821457bdfd7b591657c5c3
Tiffany <chin.tiffany108@gmail.com>
2020-07-13 19:00:05 -0400

    54321

--- src/components/techniques.js
+++ src/components/techniques.js
@@ -22,35 +22,43 @@ export default Techniques =()=> {
         },    
     ]
     const [current, setCurrent] = useState(0)
-   
     
- return(
-        <View style={styles.container}>
-        <Text style={styles.text} >5-4-3-2-1 Technique</Text>
-        <Image style={styles.paper} source={require('./images/Technique/background.png')}/>
-        <Image style={styles.image} source={techniques[current].image}/>
-           
-        { current !== 3 ?
-          <TouchableOpacity onPress={()=>setCurrent(current+1)} style={styles.rightArrow} >
-            <Image source={require('./images/misc/rightArrow.png')}/>
-          </TouchableOpacity>
-        :
-            <View style={styles.rightArrow} >
-                <Image source={require('./images/misc/disabledRight.png')}/>
+ return (
+        <View style={styles.container} >
+            <Text style={styles.text} >5-4-3-2-1 Technique</Text>
+            <Image style={styles.paper} source={require('./images/Technique/background.png')} />
+            <Image style={styles[`image${current}`]} source={techniques[current].image} />
+            { current === 2 ? 
+                <> 
+                 <Text>What do you call a bear without an ear? A bee!</Text>
+
+                <Image style={styles.ear} source={require('./images/Technique/ear.png')}/> 
+                <Text>Acknowledge THREE things you hear.</Text>
+                </>
+
+                : null
+            }
+            
+            { current !== 4 ?
+            <TouchableOpacity onPress={() => setCurrent(current + 1)} style={styles.rightArrow} >
+                <Image source={require('./images/misc/rightArrow.png')} />
+            </TouchableOpacity>
+            :
+                <View style={styles.rightArrow} >
+                    <Image source={require('./images/misc/disabledRight.png')}/>
+                </View>
+            }
+
+
+            { current !== 0 ?
+            <TouchableOpacity onPress={() => setCurrent(current - 1)} style={styles.leftArrow}>
+            <Image source={require('./images/misc/leftArrow.png')} />
+            </TouchableOpacity>
+            :
+            <View style={styles.leftArrow} >
+                <Image source={require('./images/misc/disabledLeft.png')} />
             </View>
         }
-
-
-        { current !== 0 ?
-          <TouchableOpacity onPress={()=>setCurrent(current-1)} style={styles.leftArrow}>
-           <Image  source={require('./images/misc/leftArrow.png')}/>
-           </TouchableOpacity>
-           :
-           <View style={styles.leftArrow} >
-            <Image source={require('./images/misc/disabledLeft.png')}/>
-           </View>
-       }
-
        </View>
    )
 }
--- src/stylesheets/techniques.styles.js
+++ src/stylesheets/techniques.styles.js
@@ -1,14 +1,15 @@
 import { StyleSheet } from 'react-native';
+
 export default  StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#5EB262',
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center',
     },
     text: {
         textAlign: 'center',
-        marginTop: '-10%',
+        marginTop: '5%',
         marginBottom: '5%',
         fontFamily: 'Avenir',
         fontSize: 18,
@@ -17,23 +18,63 @@ export default  StyleSheet.create({
         lineHeight: 20,
         width:300,
         fontSize: 23,
-        paddingTop: 10
+        paddingTop: 10,
     }, 
-    image:{
+    paper: {
+        position: 'absolute',
+        top: '10%',
+    },
+    // image:{
+    //     position: 'relative',
+    //     top:'22%', 
+    //     left: '22.2%',
+    // },
+    image0:{
         position: 'absolute',
         top:'22%', 
         left: '22.2%',
-    }
-    ,
+    },
+    image1:{
+        position: 'absolute',
+        top:'22%', 
+        left: '22.2%',
+    },
+    image2:{
+        height: 400,
+        width: 316,
+        position: 'absolute',
+        top:'22%', 
+        left: '8.2%',
+    },
+    ear: {
+        height: 60,
+        width: 60,
+        position: 'absolute',
+        top: '40%',
+        left: '54.2%',
+    },
+    image3:{
+        height: 370,
+        width: 258,
+        position: 'absolute',
+        top:'24%', 
+        left: '21.2%',
+    },
+    image4:{
+        height: 371,
+        width: 276,
+        position: 'absolute',
+        top:'24%', 
+        left: '18.2%',
+    },
     leftArrow: {
         position: 'absolute', 
-        top: '62%', 
+        top: '69%', 
         left: '15%'
-
     },
     rightArrow: {
         position: 'absolute', 
-        top: '62%', 
+        top: '69%', 
         right: '15%'
     }
 })
