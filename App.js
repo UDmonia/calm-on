f1d9903d68dc9d534eff1ae8551426978276e319
jackzhen1996 <jackzhen999@gmail.com>
2020-08-06 14:05:03 -0700

    fixed navbar

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { Image} from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Splash from './src/components/splash.js';
 import LoginSignup from './src/components/session/loginSignup.js';
-import Home from './src/components/home.js';
 import IntroVideo from './src/components/video.js';
 import Storytime from './src/components/storytime.js';
 import MilkMilkMilk from './src/components/milkMilkMilk';
 import Profile from './src/components/profile.js';
-import mindfulnessStack from './src/components/mindfulnessStack';
+import Mindfulness from './src/components/mindfulness';
 import spriteChat from './src/components/spriteChat';
 /* eslint-disable react/jsx-filename-extension */
 import { Provider } from "react-redux";
 import configureStore from "./src/store/store";
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import Home from './src/components/homeStack'
 
 const Stack = createStackNavigator();
 const store = configureStore();
 
-export default function App(props) {
+export default function App() {
   
   console.disableYellowBox = true; 
 
@@ -35,7 +33,7 @@ export default function App(props) {
           <Stack.Screen name='Storytime' component={Storytime} options={{headerShown: true}}/>
           <Stack.Screen name='milkMilkMilk' component={MilkMilkMilk} />
           <Stack.Screen name='Profile' component={Profile} />
-          <Stack.Screen name= 'mindfulnessStack' component = {mindfulnessStack}/>
+          <Stack.Screen name= 'Mindfulness' component = {Mindfulness}/>
           <Stack.Screen name = 'spriteChat' component = {spriteChat} options = {{
             headerShown:true, 
             headerTitle: 'Sprite',
