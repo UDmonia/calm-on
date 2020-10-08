72bfa56dc81188342693916e6126cca03bc4c7cc
Luis <ltluistrujillo25@gmail.com>
2020-10-08 09:47:08 -0700

    States to all buttons added

--- src/components/CheckInExplain.js
+++ src/components/CheckInExplain.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, TouchableOpacity, Text, Image } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { useDispatch } from "react-redux";
 import "../stylesheets/checkInExplainStyles";
+import { checkin } from "../actions/session_actions";
 
 const imgPaths = [
   {
@@ -46,24 +47,78 @@ const getPath = (feeling) => {
   return path;
 };
 
+const button = (desc) => {
+  return (
+    <TouchableOpacity
+      key={desc}
+      style={{
+        height: 55,
+        width: 322,
+        // backgroundColor: "#E5E5E5",
+        backgroundColor: desc.state ? "#ADD8E5" : "#E5E5E5",
+        borderRadius: 8,
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 10,
+      }}
+      onPress={() => {
+        desc.setState(!desc.state);
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>{desc.desc}</Text>
+    </TouchableOpacity>
+  );
+};
+
 const CheckInExplain = ({ route, navigation: { navigate } }) => {
-  const [value, onChangeText] = React.useState("Useless Placeholder");
+  const [reasons, setReasons] = useState("");
+  const [school, setSchool] = useState(false);
+  const [friends, setFriends] = useState(false);
+  const [family, setFamily] = useState(false);
+  const [health, setHealth] = useState(false);
+  const [sports, setSports] = useState(false);
+  const [other, setOther] = useState(false);
   const { feeling } = route.params;
   const img = getPath(feeling);
+  const dispatch = useDispatch();
+
+  const handleAddEmotion = (feeling, reasons) => {
+    return dispatch(
+      checkin({
+        mood: feeling,
+        journal: reasons,
+      })
+    );
+  };
+
+  const whyFeeling = [
+    { id: 1, desc: "School", state: school, setState: setSchool },
+    { id: 2, desc: "Friends", state: friends, setState: setFriends },
+    { id: 3, desc: "Family", state: family, setState: setFamily },
+    { id: 4, desc: "Health", state: health, setState: setHealth },
+    { id: 5, desc: "Sports Team", state: sports, setState: setSports },
+    { id: 6, desc: "Other Groups", state: other, setState: setOther },
+  ];
 
   return (
     <View style={styles.tmp}>
       <Image style={styles.feelingImg} source={img} />
       <Text style={styles.txtFeeling}>Today I'm feeling {feeling}!</Text>
-      <Text style={styles.txtOptional}>
-        Tell me why you're feeling {feeling}...(optional)
-      </Text>
-      <TextInput
-        style={styles.userNameInput}
-        placeholder={""}
-        onChangeText={(text) => onChangeText(text)}
-        multiline
-      />
+      <View
+        style={{
+          marginHorizontal: 40,
+          marginVertical: 20,
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontSize: 18 }}>
+          What are you feeling {feeling} about? Select as many options as you
+          want.
+        </Text>
+      </View>
+      {whyFeeling.map((desc) => {
+        return button(desc);
+      })}
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.buttons}
@@ -76,15 +131,31 @@ const CheckInExplain = ({ route, navigation: { navigate } }) => {
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.buttons}
-          onPress={() => navigate("Home")}
+          onPress={() => {
+            handleAddEmotion(
+              feeling,
+              whyFeeling
+                .filter((item) => item.state !== false)
+                .map((item) => item.desc)
+                .toString()
+            );
+            // console.log(
+            //   // whyFeeling.map((item) => {
+            //   //   return item.state ? item.desc : null;
+            //   // })
+            //   whyFeeling
+            //     .filter((item) => item.state !== false)
+            //     .map((item) => item.desc)
+            //     .toString()
+            // );
+            navigate("Home");
+          }}
         >
           <Image
             style={styles.buttonCancel}
             source={require("../../assets/checkInSubmitButton.png")}
           />
         </TouchableOpacity>
-        {/* <Button title="Cancel" onPress={() => navigate("Home")} />
-        <Button title="Submit" onPress={() => navigate("Home")} /> */}
       </View>
     </View>
   );
@@ -111,7 +182,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: "row",
-    marginTop: 100,
+    marginTop: 25,
   },
   buttons: {
     width: 126,
@@ -133,7 +204,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   feelingImg: {
-    marginTop: 75,
+    marginTop: 25,
     width: 88,
     height: 88,
   },
