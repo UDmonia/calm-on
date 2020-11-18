eeea40182a5326be5c583b3433dea840394e9cf1
Zoolyn <jerrylzhu@gmail.com>
2020-11-18 18:33:39 -0500

    Adjusted routing
    Changed route from the sprite home page:
    - OLD: home -> FlatActivities
    - NEW: home -> ChatPlaceholder

--- src/components/home.js
+++ src/components/home.js
@@ -95,7 +95,7 @@ const Home = ({ props, navigation: { navigate } }) => {
 
   function handleBtnPress() {
     if (currentSpirit === sprite) {
-      navigate("FlatActivities");
+      navigate("chatPlaceholder");
     }
   }
 
