92be6b0c115fa5b69a143de151e0af360531d945
Gabriel <gabrielmarquez1338@gmail.com>
2020-11-20 13:01:39 -0800

    Fixed emailerror

--- src/components/session/session_form.js
+++ src/components/session/session_form.js
@@ -101,6 +101,8 @@ const SessionForm = ({
 
   const handleChange = (field) => (text) => setUser({ ...user, [field]: text });
   const { email, password, confirmPassword, birthday, name } = user;
+  const emailError =
+    localErrors.find((e) => e.match(/email/)) || dbErrors.email;
   const passwordError =
     localErrors.find((e) => e.match(/password [^c]/)) || dbErrors.password;
   const confirmPasswordError = localErrors.find((e) => e.match(/match/));
