7935b27a7d2e2f2b2ea855ae29b4ec30545c3afb
Zoolyn <Zoolyn.cs@gmail.com>
2020-11-22 11:38:52 -0500

    Added back the dev version of the chatPlaceholder
    Readded the chatPlaceholder from the dev branch back after mistakingly taking it out during a merge conflict.

--- src/components/chatPlaceholder.js
+++ src/components/chatPlaceholder.js
@@ -17,33 +17,43 @@ const chatPlaceholder = ({ route, navigation: { navigate } }) => {
   const charaterActivities = curCharacter.activities;
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Placeholder page</Text>
-      <TouchableOpacity onPress={() => navigate("milkMilkMilk")}>
-        <Text>Milk Milk Milk</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("boxBreathing")}>
-        <Text>Box Breathing</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("FiveFourThreeTwoOne")}>
-        <Text>5-4-3-2-1 Technique</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("Mindfulness")}>
-        <Text>Mindfulness</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("Activities")}>
-        <Text>Activities</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("Adventure")}>
-        <Text>Going on an Adventure</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("FlatActivities")}>
-        <Text>FlatActivities</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("kpi")}>
-        <Text>kpi</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground style={styles.background} source={bg}>
+      <Image style={styles.chosenCharacter} source={img} />
+      <View style={styles.activityBtnContainer}>
+        <TouchableOpacity
+          style={styles.activityBtn}
+          onPress={() =>
+            navigate("FlatActivities", {
+              activities: charaterActivities,
+              headerColor: curCharacter.characterColor,
+            })
+          }
+        >
+          <Image source={activitiesBtnImg} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.chatContainer}>
+        <Text>Placeholder page</Text>
+        <TouchableOpacity onPress={() => navigate("milkMilkMilk")}>
+          <Text>Milk Milk Milk</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigate("boxBreathing")}>
+          <Text>Box Breathing</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigate("FiveFourThreeTwoOne")}>
+          <Text>5-4-3-2-1 Technique</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigate("Mindfulness")}>
+          <Text>Mindfulness</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigate("Activities")}>
+          <Text>Activities</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigate("Adventure")}>
+          <Text>Going on an Adventure</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
