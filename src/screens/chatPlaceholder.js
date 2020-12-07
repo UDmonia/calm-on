f2ef065d13d33a0ca307e7d7205c695a0664bebb
Luis <ltluistrujillo25@gmail.com>
2020-12-07 14:50:38 -0800

    Initial working character chat

--- src/screens/chatPlaceholder.js
+++ src/screens/chatPlaceholder.js
@@ -1,4 +1,3 @@
-//This file is a temporary placeholder
 import {
   Text,
   View,
@@ -6,8 +5,106 @@ import {
   ImageBackground,
   Image,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../stylesheets/chatPlaceholderStyles";
+
+/**
+ * TODO:
+ * [ ] Use the navigate function out of the main function
+ *    - use a string, that will be used by a switch statement to call specific fucntions
+ * [ ] Pass the route params out of the main function scope
+ * [X] Convert all of the class methods to regular functions
+ * [ ] Import the styles from the milkmilk chat box
+ * [ ] Obatain the last emotion entered
+ */
+const spriteHappy = {
+  key: null,
+  question:
+    "Hey _____. I heard you were feeling happy today. Can I tell you a story about the last time I felt happy?",
+  answers: ["Ok"],
+  animation: null,
+  renderAnim: "",
+  func: null,
+  nxtNode: [
+    {
+      key: "Ok",
+      question:
+        "Last weekend, my buddy Flynn and I went on a nice and long hike near Fairylantis! We went on a very scenic trail with a lot of trees. You could see the bridge that connects from Central Fairyland to Northern Fairyland. There was a huuuuge rainbow and it was so beautiful. I felt so happy!",
+      answers: [
+        "Wow Sprite! That sounds like so much fun! I would like to go there sometime.",
+      ],
+      animation: null,
+      renderAnim: "",
+      func: null,
+      nxtNode: [
+        {
+          key:
+            "Wow Sprite! That sounds like so much fun! I would like to go there sometime.",
+          question:
+            "It is great to feel happy, but sometimes there may be a time where you may not feel happy. Why don’t we do some activities to help you be prepared for those moments?",
+          answers: ["Sure!", "No, not right now."],
+          animation: null,
+          renderAnim: "",
+          func: null,
+          nxtNode: [
+            {
+              key: "Sure!",
+              question: "Great, let’s go!",
+              answers: ["5-4-3-2-1", "Box Breathing"],
+              animation: null,
+              renderAnim: "",
+              func: null,
+              nxtNode: [
+                {
+                  key: "5-4-3-2-1",
+                  question: "",
+                  answers: [],
+                  animation: null,
+                  renderAnim: "",
+                  func: () =>
+                    navigation.navigate("FiveFourThreeTwoOneTech", {
+                      headerColor: curCharacter.characterColor,
+                    }),
+                  nxtNode: [],
+                },
+                {
+                  key: "Box Breathing",
+                  question: "",
+                  answers: [],
+                  animation: null,
+                  renderAnim: "",
+                  func: () =>
+                    navigation.navigate("boxBreathing", {
+                      headerColor: curCharacter.characterColor,
+                    }),
+                  nxtNode: [],
+                },
+              ],
+            },
+            {
+              key: "No, not right now.",
+              question:
+                "Do any of these pop up in your mind when we say milk? You can choose one of the options above:",
+              answers: [],
+              animation: null,
+              renderAnim: "",
+              func: () =>
+                navigation.navigate("FlatActivities", {
+                  activities: charaterActivities,
+                  headerColor: curCharacter.characterColor,
+                }),
+              nxtNode: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+function findNode(answer, nxtNode) {
+  return nxtNode.find((node) => node.key === answer);
+}
 
 const chatPlaceholder = ({ route, navigation: { navigate } }) => {
   const { curCharacter } = route.params;
@@ -15,6 +112,25 @@ const chatPlaceholder = ({ route, navigation: { navigate } }) => {
   const bg = curCharacter.background;
   const activitiesBtnImg = curCharacter.viewActivities;
   const charaterActivities = curCharacter.activities;
+
+  const [question, setQuestion] = useState(spriteHappy.question);
+  const [answers, setAnswers] = useState(spriteHappy.answers);
+  const [nxtNode, setNxtNode] = useState(spriteHappy.nxtNode);
+
+  function traverseTree(answers) {
+    const newNode = findNode(answers, nxtNode);
+    if (newNode !== undefined) {
+      setQuestion(() => newNode.question);
+      setAnswers(() => newNode.answers);
+      setNxtNode(() => newNode.nxtNode);
+    } else {
+      console.log("done with the activity.");
+      navigate("FlatActivities", {
+        activities: charaterActivities,
+        headerColor: curCharacter.characterColor,
+      });
+    }
+  }
 
   return (
     <ImageBackground style={styles.background} source={bg}>
@@ -32,38 +148,33 @@ const chatPlaceholder = ({ route, navigation: { navigate } }) => {
           <Image source={activitiesBtnImg} />
         </TouchableOpacity>
       </View>
-      <View style={styles.chatContainer}>
-        <Text>Placeholder page</Text>
-        <TouchableOpacity onPress={() => navigate("milkMilkMilk")}>
-          <Text>Milk Milk Milk</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigate("boxBreathing")}>
-          <Text>Box Breathing</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() =>
-            navigate("IntroActivity", {
-              name: "54321 Technique",
-              actNav: "FiveFourThreeTwoOneTech",
-              about:
-                "Use your senses to bring you back to the present moment. This will help you feel more focused and calm.",
-              helpful:
-                "You have trouble focusing or when you feel scared or worried.",
-              img: require("../../assets/FiveFourThreeTwoOne/54321Title.png"),
-            })
-          }
+      <View style={styles.box}>
+        <View
+          style={[styles.top, { backgroundColor: curCharacter.characterColor }]}
         >
-          <Text>5-4-3-2-1 Technique</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigate("Mindfulness")}>
-          <Text>Mindfulness</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigate("Activities")}>
-          <Text>Activities</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigate("Adventure")}>
-          <Text>Going on an Adventure</Text>
-        </TouchableOpacity>
+          <Text style={styles.question}>{question}</Text>
+        </View>
+        <View style={styles.bottom}>
+          {answers.map((a, i) => {
+            return (
+              <TouchableOpacity
+                key={i}
+                style={[
+                  styles.answer,
+                  { borderColor: curCharacter.characterColor },
+                ]}
+                onPress={() => {
+                  // func;
+                  traverseTree(a);
+                }}
+              >
+                <Text key={i} style={styles.a}>
+                  {a}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
       </View>
     </ImageBackground>
   );
--- src/stylesheets/chatPlaceholderStyles.js
+++ src/stylesheets/chatPlaceholderStyles.js
@@ -1,4 +1,7 @@
 import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+
+let screenWidth = Dimensions.get("window").width;
 
 export default StyleSheet.create({
   background: {
@@ -23,5 +26,55 @@ export default StyleSheet.create({
     width: "90%",
     backgroundColor: "white",
     borderRadius: 10,
+  },
+  top: {
+    // backgroundColor: "#DD6755",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    minHeight: 50,
+    width: "100%",
+  },
+  question: {
+    color: "#FFFFFF",
+    fontFamily: "FontReg",
+    fontSize: 14,
+    fontWeight: "800",
+    textAlign: "center",
+    marginVertical: 17,
+    marginHorizontal: 14,
+  },
+  box: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+    minHeight: 100,
+    width: 330,
+    position: "absolute",
+    marginHorizontal: (screenWidth - 330) / 2,
+    bottom: "5%",
+    zIndex: 3,
+  },
+  bottom: {
+    marginTop: 10,
+    borderBottomEndRadius: 10,
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+  },
+  a: {
+    fontFamily: "FontReg",
+    fontWeight: "800",
+    textAlign: "center",
+    lineHeight: 34,
+  },
+  answer: {
+    backgroundColor: "#FFFFFF",
+    borderWidth: 2,
+    borderRadius: 10,
+    fontSize: 14,
+    height: 35,
+    width: 284,
+    textAlign: "center",
+    marginBottom: "5%",
   },
 });
