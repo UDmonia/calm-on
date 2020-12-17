e08cd77cc316bbea35b9793592860b13fc5ac338
jackzhen1996 <jackzhen999@gmail.com>
2020-12-16 20:18:57 -0800

    pull from dev

---
+++ src/stylesheets/hexCodes.js
@@ -0,0 +1,54 @@
+/*
+Convention:
+'color': {
+    'colorName': 'hex code'   
+}
+*/
+
+
+const hexCodes = {
+    //Colors associated with moods in check-in
+    'moodColors':{
+        'happy':"#FBC423",
+        'excited':"#AED4B0",
+        'angry':"#F09696",
+        'scared':"#A5DFF0",
+        'worried':"#E8B285",
+        'sad':"#DF9AFF",
+        'empty':'' //'Tell me how you're feeling button'
+    },
+    'characterColors':{
+        'greenSprite':"#2E7D32",
+        'brownFlynn':"#B24A2B",
+        'purpleAurora':"#8248D7"
+    },
+    'SpriteActivityColors':{
+        'techniques':"#A7D1A8",
+        'boxBreathing':"#418295",
+        'counting':"#FBBDB4",
+        'milk':"#6E891A",
+        'picnic':"#DA71C4"
+    },
+    'dialog':{
+        'question':"#DD6755",
+        'answerBackground':"#FBBDB4"
+    },
+
+    'calendar':{
+        'brown':'#873E25',
+        'headerYellow':'#FFC10E',
+        'headerFont':'#424242'
+    },
+
+    'boxBreathing':{
+        'blue':"#429BAF",
+        'darkBlue':"#064B5B",
+        'text':"#064B5B"
+    },
+    'bottomNav':{
+        'background':'#E2E8F8',
+        'dash':'#4FE80F'
+    }
+}
+
+export default hexCodes;
