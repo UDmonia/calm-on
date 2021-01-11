03bf2e856f6ae5c42a5fa2945c26769e716959d0
Luis <ltluistrujillo25@gmail.com>
2021-01-11 14:26:26 -0800

    Removing my ip

--- src/util/session_util.js
+++ src/util/session_util.js
@@ -9,13 +9,10 @@ const SessionAPI = {
     }
   },
   register: (user) =>
-    axios.post(`http://192.168.1.147:3001/api/users/register`, user),
-  login: (user) =>
-    axios.post(`http://192.168.1.147:3001/api/users/login`, user),
-  addName: (user) =>
-    axios.put(`http://192.168.1.147:3001/api/users/name`, user),
-  checkin: (user) =>
-    axios.put(`http://192.168.1.147:3001/api/users/checkin`, user),
+    axios.post(`http://localhost:3001/api/users/register`, user),
+  login: (user) => axios.post(`http://localhost:3001/api/users/login`, user),
+  addName: (user) => axios.put(`http://localhost:3001/api/users/name`, user),
+  checkin: (user) => axios.put(`http://localhost:3001/api/users/checkin`, user),
 };
 
 export default SessionAPI;
