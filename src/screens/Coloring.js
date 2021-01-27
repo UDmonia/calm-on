9e2dcebb96e654f30eaad48dcf5beddc5623c1cd
Jerry <zoolyn.cs@gmail.com>
2021-01-27 17:52:02 -0500

    Added Buttons
    Added some buttons that will eventually navigate to the individual coloring pages

--- src/screens/Coloring.js
+++ src/screens/Coloring.js
@@ -6,13 +6,66 @@ import {
     Image,
     ImageBackground,
     SafeAreaView,
+    FlatList,
   } from "react-native";
 import styles from "../stylesheets/coloringStyles";
+import Exit from "../components/Exit";
+
+const DATA = [
+    {
+        id: 1,
+        title: "picture1"
+    },
+    {
+        id: 2,
+        title: "picture2",
+    },
+    {
+        id: 3,
+        title: "picture3",
+    },
+    {
+        id: 4,
+        title: "picture3",
+    },
+    {
+        id: 5,
+        title: "picture3",
+    },
+    {
+        id: 6,
+        title: "picture3",
+    }
+]
+
+
+function ImageBox(data) {
+    return(
+        <TouchableOpacity style={styles.imageBoxContainer}>
+            <Image 
+                source={require('../../assets/favicon.png')}
+                style={styles.img}/>
+        </TouchableOpacity>
+    );
+}
 
 export default function Coloring (){
     return(
         <SafeAreaView style={styles.container}>
+            <View style={styles.exitContainer}>
+                <Exit />
+            </View>
+            <View style={styles.main}>
+                <FlatList 
+                    data={DATA}
+                    renderItem={item => ImageBox(item)}
+                    keyExtractor={item => item.id}
+                    numColumns={2}
+                    style={styles.imageList}/>
+            </View>
+            <View style={styles.bottom}>
 
+            </View>
         </SafeAreaView>
     );
 }
--- src/stylesheets/coloringStyles.js
+++ src/stylesheets/coloringStyles.js
@@ -1,9 +1,40 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform, StatusBar } from "react-native";
+import {windowWidth, windowHeight } from "../util/windowDimensions";
 import hex from "./hexCodes";
 
 export default StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "grey",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
+    exitContainer: {
+        paddingLeft: "5%",
+        paddingTop: "5%",
+    },
+    main: {
+        flex: 3,
+        backgroundColor: "lightblue",
+    },
+    bottom: {
+        flex: 1,
+        backgroundColor: "lightgreen",
+    },
+    imageList: {
+        flex: 1,
+    },
+    imageBoxContainer: {
+        flex: 1,
+        margin: "5%",
+        maxWidth: "40%",
+        backgroundColor: "white",
+        borderWidth: windowWidth * 0.01,
+        borderColor: hex.purple.purple2,
+        borderRadius: windowWidth * 0.01
+    },
+    img: {
+        height: windowHeight * 0.25,
+        width: "100%",
+        resizeMode: "contain",
     }
 });
