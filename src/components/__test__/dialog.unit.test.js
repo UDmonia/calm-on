02635bd008d51e4999bb73f5cf9b03e0d053c3df
hmongkey12 <khammadaolee@gmail.com>
2021-03-12 09:56:06 -0800

    removed snapshot from gitignore

--- src/components/__test__/dialog.unit.test.js
+++ src/components/__test__/dialog.unit.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 import Dialog from "../dialog";
-import { interpolate } from "react-native-reanimated";
 
 describe("Dialog Test Suite", () => {
   test("Dialog snapshot matches", () => {
