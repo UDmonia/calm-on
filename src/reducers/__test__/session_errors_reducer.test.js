01698e22bfa0b5485e19fb500fa66a5a6272f213
hmongkey12 <khammadaolee@gmail.com>
2021-03-12 09:06:21 -0800

    added unit test for session_reducer

--- src/reducers/__test__/session_errors_reducer.test.js
+++ src/reducers/__test__/session_errors_reducer.test.js
@@ -2,20 +2,18 @@ import { INTERRUPTION_MODE_ANDROID_DO_NOT_MIX } from "expo-av/build/Audio";
 import Reducer from "../session_errors_reducer";
 
 describe("Session Errors Reducer Test", () => {
+  const state = { user: "Bob" };
   test("should return empty state", () => {
-    expect(Reducer({ user: "bob" }, { type: "RECEIVE_USER" })).toEqual({});
+    expect(Reducer(state, { type: "RECEIVE_USER" })).toEqual({});
   });
 
   test("Should return initial state", () => {
-    expect(Reducer({ user: "bob" }, { type: "" })).toEqual({ user: "bob" });
+    expect(Reducer(state, { type: "" })).toEqual({ user: "Bob" });
   });
 
   test("Should return error", () => {
     expect(
-      Reducer(
-        { user: "bob" },
-        { type: "RECEIVE_SESSION_ERRORS", errors: "Error!" }
-      )
+      Reducer(state, { type: "RECEIVE_SESSION_ERRORS", errors: "Error!" })
     ).toEqual("Error!");
   });
 });
