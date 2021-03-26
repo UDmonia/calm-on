1335edb052cbd7650c69a266cf87b4f185e13c8f
Jerry <zoolyn.cs@gmail.com>
2021-03-26 00:33:27 -0400

    Added story
    Need to add all the stories and finish the styles

--- src/screens/AdventureLocationMadLib.js
+++ src/screens/AdventureLocationMadLib.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, TouchableOpacity, SafeAreaView, Image, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+  ImageBackground,
+} from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { navigate } from "../components/RootNavigation";
 import Exit from "../components/Exit";
@@ -18,14 +25,44 @@ const AdventureLocationMadLib = ({ route }) => {
   };
 
   return (
-    <ImageBackground source={bg} style={{height: "100%", width: "100%"}}>
+    <ImageBackground source={bg} style={{ height: "100%", width: "100%" }}>
       <SafeAreaView style={styles.mainContainer}>
         <View style={styles.exitContainer}>
-          <Exit style={{alignSelf: "flex-start"}}/>
+          <Exit style={{ alignSelf: "flex-start" }} />
         </View>
-        <View style={{flex: 1,width:windowWidth * .9}}>
-          <ScrollView bounces={false} contentContainerStyle={styles.storyContainer} showsVerticalScrollIndicator={false}>
-            <Text style={styles.storyText}> Dear Diary,{"\n"} It was a beautiful day outside.  There were birds chirping and the sun was shining bright. I wanted to go on a picnic with my friends, but they were busy so I went with mom. Dad said he was too busy doing boring grown-up stuff. I took an <Image source={randomWords[0].img} />, my <Image source={randomWords[1].img} />, my <Image source={randomWords[2].img} />, and my stuffed <Image source={randomWords[3].img} />.  When I got there, I sat on my <Image source={randomWords[4].img} /> with mom and saw people drawing <Image source={randomWords[5].img} />. There was a farm near the park which had <Image source={randomWords[6].img} />, <Image source={randomWords[7].img} />, and <Image source={randomWords[8].img} />. After visiting the farm, we started to play various games such as hopscotch. We spent an hour at the swings and then mom said we had to go home.  It was a good day overall. I hope tomorrow will be a good day as well.</Text>
+        <View style={{ flex: 1, width: windowWidth * 0.9 }}>
+          <ScrollView
+            bounces={false}
+            contentContainerStyle={styles.storyContainer}
+            showsVerticalScrollIndicator={false}
+          >
+            <Text style={styles.storyText}>
+              {" "}
+              Dear Diary,{"\n"} It was a beautiful day outside. There were birds
+              chirping and the sun was shining bright. I wanted to go on a
+              picnic with my friends, but they were busy so I went with mom. Dad
+              said he was too busy doing boring grown-up stuff. I took an{" "}
+              <Image style={styles.inlineImage} source={randomWords[0].img} />,
+              my{" "}
+              <Image style={styles.inlineImage} source={randomWords[1].img} />,
+              my{" "}
+              <Image style={styles.inlineImage} source={randomWords[2].img} />,
+              and my stuffed{" "}
+              <Image style={styles.inlineImage} source={randomWords[3].img} />.
+              When I got there, I sat on my{" "}
+              <Image style={styles.inlineImage} source={randomWords[4].img} />{" "}
+              with mom and saw people drawing{" "}
+              <Image style={styles.inlineImage} source={randomWords[5].img} />.
+              There was a farm near the park which had{" "}
+              <Image style={styles.inlineImage} source={randomWords[6].img} />,{" "}
+              <Image style={styles.inlineImage} source={randomWords[7].img} />,
+              and{" "}
+              <Image style={styles.inlineImage} source={randomWords[8].img} />.
+              After visiting the farm, we started to play various games such as
+              hopscotch. We spent an hour at the swings and then mom said we had
+              to go home. It was a good day overall. I hope tomorrow will be a
+              good day as well.
+            </Text>
           </ScrollView>
         </View>
         <TouchableOpacity onPress={() => handlePress()}>
--- src/stylesheets/adventureLocationMadLibStyles.js
+++ src/stylesheets/adventureLocationMadLibStyles.js
@@ -3,8 +3,8 @@ import hex from "./hexCodes";
 import { windowWidth, windowHeight } from "../util/windowDimensions";
 import hexCodes from "./hexCodes";
 
-const INLINE_TEXT_SIZE = windowWidth * 0.05
-const INLINE_IMAGE_SIZE = INLINE_TEXT_SIZE * 0.25
+const INLINE_TEXT_SIZE = windowWidth * 0.05;
+const INLINE_IMAGE_SIZE = INLINE_TEXT_SIZE * 1.75;
 
 export default StyleSheet.create({
   mainContainer: {
@@ -17,7 +17,7 @@ export default StyleSheet.create({
   exitContainer: {
     width: windowWidth * 0.9,
     height: windowHeight * 0.1,
-    justifyContent: "center"
+    justifyContent: "center",
   },
   storyContainer: {
     backgroundColor: hexCodes.green.green1,
@@ -26,12 +26,11 @@ export default StyleSheet.create({
     borderRadius: 10,
   },
   inlineImage: {
-    flex: 1,
-    width: null,
-    height: null,
-    resizeMode: 'contain'
+    height: INLINE_IMAGE_SIZE,
+    width: INLINE_IMAGE_SIZE,
+    resizeMode: "contain",
   },
   storyText: {
     fontSize: INLINE_TEXT_SIZE,
-  }
+  },
 });
