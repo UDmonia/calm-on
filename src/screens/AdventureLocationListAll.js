4ceb4a6bbf49090fca820a70b4ac62f748c34d9f
Zoolyn <Zoolyn.cs@gmail.com>
2021-04-05 18:18:44 -0400

    Adding Madlib feature to the Choose Your Adventure Activity  (#85)
    * Adventure MadLib boilerplate
    
    * Added List of all Items boilerplate
    
    * Finished most of the functional code
    
    Need to tune up the styles and refactor
    NOTE: Seems like scrollview can't be made clickable, will have to do more research on that
    
    * Added story
    
    Need to add all the stories and finish the styles
    
    * Picnic stories done
    
    Redid the madlib pages to me click to view rest of the story whereas the previous version just had a scrollview
    Still need to do the stories for the Park and Movie Theater
    
    * Added all stories and adjusted the styles
    
    Added the rest of the stories for the different locations and adjusted the styles to match the design guide.
    Waiting on the design team on certain stories having too little or too many random words.
    
    * Fixed some of the visual bugs a the beginning of the activity
    
    Added the dynamic name in the dialog box and changed the color to the dark green color
    
    * Changed to scollview
    
    changed the madlib to be displayed as a scrollview and thus created a next button for the user to the next page
    
    * Adjusted more styling and got rid of some unused code
    
    * changed the list to a grid and fixed up some the styles
    
    This should be the last major adjustment, the only thing we need to do is make sure that the sun asset is updated and made smaller.
    
    * More style adjusting
    
    * Fixed Sun icon
    
    shrank the sun icon down

---
+++ src/screens/AdventureLocationListAll.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  SafeAreaView,
+  ImageBackground,
+  Modal,
+} from "react-native";
+import { navigate } from "../components/RootNavigation";
+import styles from "../stylesheets/adventureLocationListAllStyles";
+import Exit from "../components/Exit";
+const AdventureLocationListAll = ({ route }) => {
+  const { pMsg, sMsg, arr, bg, bgTint, location } = route.params;
+  const randomStory = Math.floor(Math.random() * 3);
+  const [randomWords, setRandomWords] = useState([]);
+  useEffect(() => {
+    let i;
+    let tempArray = [];
+    while (tempArray.length < 9) {
+      let r = Math.floor(Math.random() * 26);
+      if (tempArray.indexOf(r) === -1) {
+        tempArray.push(r);
+      }
+    }
+    // console.log(tempArray);
+    for (i = 0; i < 9; i++) {
+      tempArray[i] = arr[tempArray[i]];
+    }
+    // console.log(tempArray);
+    setRandomWords(tempArray);
+  }, []);
+
+  const handlePress = () => {
+    navigate("AdventureLocationMadLib", {
+      bg: bg,
+      bgTint: bgTint,
+      pMsg: pMsg,
+      sMsg: sMsg,
+      randomWords: randomWords,
+      randomStory: randomStory,
+      location: location,
+    });
+  };
+
+  return (
+    <ImageBackground source={bgTint} style={styles.background}>
+      <SafeAreaView style={styles.mainContainer}>
+        <View style={styles.exitContainer}>
+          <Exit
+            navTo={"Modal"}
+            img={require("../../assets/exit/whtExitBtn.png")}
+          />
+        </View>
+        <ScrollView contentContainerStyle={styles.allSelectedContainer}>
+          <View>
+            <Text style={styles.selectedItemsTxt}>Selected Items</Text>
+          </View>
+          <View style={styles.grid}>
+            {arr.map((item) => {
+              return (
+                <View key={item.name} style={styles.itemContainer}>
+                  <Image source={item.img} style={styles.imgContainer} />
+                  <Text style={styles.itemsTxt}>{item.name}</Text>
+                </View>
+              );
+            })}
+          </View>
+          <TouchableOpacity
+            style={styles.nextButton}
+            onPress={() => handlePress()}
+          >
+            <Text style={styles.nextButtonTxt}>Next</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+};
+
+export default AdventureLocationListAll;
---
+++ src/screens/AdventureLocationMadLib.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+  ImageBackground,
+} from "react-native";
+import Exit from "../components/Exit";
+import styles from "../stylesheets/adventureLocationMadLibStyles";
+import hexCodes from "../stylesheets/hexCodes";
+import { windowHeight, windowWidth } from "../util/windowDimensions";
+import { navigate } from "../components/RootNavigation";
+import AdventureMadLib from "../components/AdventureMadLib";
+const AdventureLocationMadLib = ({ route }) => {
+  const {
+    pMsg,
+    sMsg,
+    bg,
+    bgTint,
+    randomWords,
+    randomStory,
+    location,
+  } = route.params;
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <ImageBackground source={bg} style={{ height: "100%", width: "100%" }}>
+      <SafeAreaView style={styles.mainContainer}>
+        <View style={styles.exitContainer}>
+          <Exit
+            style={{ alignSelf: "flex-start" }}
+            navTo={"Modal"}
+            img={require("../../assets/exit/whtExitBtn.png")}
+          />
+        </View>
+        <View style={styles.storyContainer}>
+          {/* If you want to edit/add madlibs you need to access the AdventureMadLib component located in components/AdventureMadLib.js */}
+          <AdventureMadLib
+            randomWords={randomWords}
+            randomStory={randomStory}
+            location={location}
+            progress={progress}
+            setProgress={setProgress}
+            pMsg={pMsg}
+            sMsg={sMsg}
+            bg={bgTint}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={styles.nextButton}
+            onPress={() =>
+              navigate("kpi", {
+                bg: bgTint,
+                pMsg: pMsg,
+                sMsg: sMsg,
+              })
+            }
+          >
+            <Text style={styles.nextButtonText}>Next</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+};
+
+export default AdventureLocationMadLib;
