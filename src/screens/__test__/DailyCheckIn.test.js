60c58c63efd7dcd33bc2dcde4019da79986f89ba
hmongkey12 <khammadaolee@gmail.com>
2021-04-09 11:41:03 -0700

    fixed testing issues

--- src/screens/__test__/DailyCheckIn.test.js
+++ src/screens/__test__/DailyCheckIn.test.js
@@ -17,11 +17,12 @@ const Stack = createStackNavigator();
 //removes the Animated: `useNativeDriver` is not supported warning
 //useNativeDriver is only important when running code on device
 jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
+const mockDispatch = jest.fn();
 
 jest.mock("react-redux", () => ({
   ...jest.requireActual("react-redux"),
   useSelector: jest.fn(),
-  useDispatch: jest.fn(),
+  useDispatch: () => mockDispatch,
 }));
 
 jest.mock("../../actions/session_actions", () => ({
@@ -36,103 +37,103 @@ describe("DailyCheckIn Screen Tests", () => {
   afterEach(() => {
     fetchMock.restore();
     useSelector.mockClear();
-    useDispatch.mockClear();
+    mockDispatch.mockClear();
     cleanup();
   });
 
   test("Dispatch and UseSelector are not called", () => {
     render(<DailyCheckIn navigation={{ navigate: jest.fn() }} />);
     expect(useSelector).toHaveBeenCalledTimes(1);
-    expect(useDispatch).toHaveBeenCalled();
+    expect(mockDispatch).not.toHaveBeenCalled();
   });
 
   test("UseDispatch is called when emotion is clicked", () => {
-    expect(useDispatch).not.toHaveBeenCalled();
+    expect(mockDispatch).not.toHaveBeenCalled();
     const { queryByTestId } = render(
       <DailyCheckIn navigation={{ navigate: jest.fn() }} />
     );
     fireEvent.press(queryByTestId("angry-button"));
-    expect(useDispatch).toHaveBeenCalled();
+    expect(mockDispatch).not.toHaveBeenCalled();
     fireEvent.press(queryByTestId("scared-button"));
-    expect(useDispatch).toHaveBeenCalled();
+    expect(mockDispatch).not.toHaveBeenCalled();
     fireEvent.press(queryByTestId("curFeeling-button"));
-    expect(useDispatch).toHaveBeenCalled();
+    expect(mockDispatch).toHaveBeenCalled();
   });
 
-  // describe("DailyCheckIn Feelings are Dispatched", () => {
-  //   test("useDispatch is called for Happy Feeling", () => {
-  //     expect(useDispatch).not.toHaveBeenCalled();
-  //     const { queryByTestId } = render(
-  //       <DailyCheckIn navigation={{ navigate: jest.fn() }} />
-  //     );
-  //     expect(useDispatch).not.toHaveBeenCalled();
-  //     fireEvent.press(queryByTestId("happy-button"));
-  //     expect(useDispatch).not.toHaveBeenCalled();
-  //     fireEvent.press(queryByTestId("curFeeling-button"));
-  //     expect(useDispatch).toHaveBeenCalledTimes(1);
-  //     expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Happy" });
-  //   });
-    // test("useDispatch is called for Excited Feeling", () => {
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   const { queryByTestId } = render(
-    //     <DailyCheckIn navigation={{ navigate: jest.fn() }} />
-    //   );
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("excited-button"));
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("curFeeling-button"));
-    //   expect(useDispatch).toHaveBeenCalledTimes(1);
-    //   expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Excited" });
-    // });
-    // test("useDispatch is called for Scared Feeling", () => {
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   const { queryByTestId } = render(
-    //     <DailyCheckIn navigation={{ navigate: jest.fn() }} />
-    //   );
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("scared-button"));
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("curFeeling-button"));
-    //   expect(useDispatch).toHaveBeenCalledTimes(1);
-    //   expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Scared" });
-    // });
-    // test("useDispatch is called for Worried Feeling", () => {
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   const { queryByTestId } = render(
-    //     <DailyCheckIn navigation={{ navigate: jest.fn() }} />
-    //   );
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("worried-button"));
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("curFeeling-button"));
-    //   expect(useDispatch).toHaveBeenCalledTimes(1);
-    //   expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Worried" });
-    // });
-    // test("useDispatch is called for Sad Feeling", () => {
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   const { queryByTestId } = render(
-    //     <DailyCheckIn navigation={{ navigate: jest.fn() }} />
-    //   );
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("sad-button"));
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("curFeeling-button"));
-    //   expect(useDispatch).toHaveBeenCalledTimes(1);
-    //   expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Sad" });
-    // });
-    // test("useDispatch is called for Angry Feeling", () => {
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   const { queryByTestId } = render(
-    //     <DailyCheckIn navigation={{ navigate: jest.fn() }} />
-    //   );
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("angry-button"));
-    //   expect(useDispatch).not.toHaveBeenCalled();
-    //   fireEvent.press(queryByTestId("curFeeling-button"));
-    //   expect(useDispatch).toHaveBeenCalledTimes(1);
-    //   expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Angry" });
-    // });
-  // });
+  describe("DailyCheckIn Feelings are Dispatched", () => {
+    test("useDispatch is called for Happy Feeling", () => {
+      expect(mockDispatch).not.toHaveBeenCalled();
+      const { queryByTestId } = render(
+        <DailyCheckIn navigation={{ navigate: jest.fn() }} />
+      );
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("happy-button"));
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("curFeeling-button"));
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Happy" });
+    });
+    test("useDispatch is called for Excited Feeling", () => {
+      expect(mockDispatch).not.toHaveBeenCalled();
+      const { queryByTestId } = render(
+        <DailyCheckIn navigation={{ navigate: jest.fn() }} />
+      );
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("excited-button"));
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("curFeeling-button"));
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Excited" });
+    });
+    test("useDispatch is called for Scared Feeling", () => {
+      expect(mockDispatch).not.toHaveBeenCalled();
+      const { queryByTestId } = render(
+        <DailyCheckIn navigation={{ navigate: jest.fn() }} />
+      );
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("scared-button"));
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("curFeeling-button"));
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Scared" });
+    });
+    test("useDispatch is called for Worried Feeling", () => {
+      expect(mockDispatch).not.toHaveBeenCalled();
+      const { queryByTestId } = render(
+        <DailyCheckIn navigation={{ navigate: jest.fn() }} />
+      );
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("worried-button"));
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("curFeeling-button"));
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Worried" });
+    });
+    test("useDispatch is called for Sad Feeling", () => {
+      expect(mockDispatch).not.toHaveBeenCalled();
+      const { queryByTestId } = render(
+        <DailyCheckIn navigation={{ navigate: jest.fn() }} />
+      );
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("sad-button"));
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("curFeeling-button"));
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Sad" });
+    });
+    test("useDispatch is called for Angry Feeling", () => {
+      expect(mockDispatch).not.toHaveBeenCalled();
+      const { queryByTestId } = render(
+        <DailyCheckIn navigation={{ navigate: jest.fn() }} />
+      );
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("angry-button"));
+      expect(mockDispatch).not.toHaveBeenCalled();
+      fireEvent.press(queryByTestId("curFeeling-button"));
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(checkin).toHaveBeenCalledWith({ journal: "", mood: "Angry" });
+    });
+  });
 
   describe("DailyCheckIn can navigate to Home", () => {
     test("DailyCheckIn Renders", () => {
