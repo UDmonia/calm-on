0974b63ceaa4d3e5654f8365838e78cf1e0f48e2
Gabriel <gabrielmarquez1338@gmail.com>
2021-08-13 19:25:08 -0700

    reverted session util

--- src/util/session_util.js
+++ src/util/session_util.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import config from "../../config"
 
 const SessionAPI = {
   setAuthToken: (token) => {
@@ -10,11 +9,11 @@ const SessionAPI = {
     }
   },
   register: (user) =>
-    axios.post(`${config.API_ACCOUNTS_URL}/api/accounts/signup`, user),
-  login: (user) => axios.post(`${config.API_ACCOUNTS_URL}/api/accounts/signin`, user),
-  addName: (user) => axios.put(`${config.API_ACCOUNTS_URL}/api/accounts/profile`, user),
+    axios.post(`http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/signup`, user),
+  login: (user) => axios.post(`http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/signin`, user),
+  addName: (user) => axios.put(`http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/profile`, user),
   checkin: (user) => axios.put(`http://localhost:3001/api/users/checkin`, user),
-  editProfile: (user) => axios.put(`${config.API_ACCOUNTS_URL}/api/accounts/profile', user`),
+  editProfile: (user) => axios.put('http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/profile', user),
 };
 
 export default SessionAPI;
