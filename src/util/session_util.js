c291bdabd952853a80f5ba91e13b1926eaca75c9
jackzhen1996 <jackzhen999@gmail.com>
2021-12-12 18:06:07 -0800

    changed to async/await

--- src/util/session_util.js
+++ src/util/session_util.js
@@ -16,9 +16,9 @@ const SessionAPI = {
   register: async (user) =>
     await axios.post(`http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/signup`, user),
   login: async (user) => await axios.post(`http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/signin`, user),
-  addName: (user) => axios.put(`http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/profile`, user),
-  checkin: (user) => axios.put(`http://localhost:3001/api/users/checkin`, user),
-  editProfile: (user) => axios.put('http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/profile', user),
+  addName: async (user) => await axios.put(`http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/profile`, user),
+  checkin: async (user) => await axios.put(`http://localhost:3001/api/users/checkin`, user),
+  editProfile: async (user) => await axios.put('http://dev-1020237653.us-west-1.elb.amazonaws.com/api/accounts/profile', user),
 };
 
 export default SessionAPI;
