e30c418d098456273388e2ae56a3cd47d489706a
Jack Zhen <63031182+jackzhen1996@users.noreply.github.com>
2022-01-09 18:16:50 -0800

    used hosted url

--- src/util/cashShop_util.js
+++ src/util/cashShop_util.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// Todo(jack): we should be storing Urls in like a React .env or something like that
+const getUrl = (action) => `http://ec2-34-215-150-120.us-west-2.compute.amazonaws.com/api/cashshop/${action}`
+
 const CashShopAPI = {
   setAuthToken: (token) => {
     if (token) {
@@ -10,16 +13,16 @@ const CashShopAPI = {
   },
   // checkout route
   checkout: async (outfitList,total) => await axios.post('...',{outfitList,total}),
-  // get coins route
-  fetchUser: async (userId)=> await axios.get('http://localhost:4000/api/cashshop/fetchUser', {params:{userId}}),
+  // get back a registered user
+  fetchUser: async (userId)=> await axios.get(getUrl('fetchUser'), {params:{userId}}),
   // get equipped route
   getEquipped: async ()=> await axios.get('...'),
   // equip or unequip route
   equippedSnapshot: async (outfitList)=> await axios.put('...', outfitList),
   // get bought items route
   getBought: async ()=> await axios.get('....'),
-  // setup user upon registering
-  setupUser: async (userId) => await axios.post('http://localhost:4000/api/cashshop/setupUser', {userId})
+  // setup user in cashshop api
+  setupUser: async (userId) => await axios.post(getUrl('setupUser'), {userId})
 };
 
 export default CashShopAPI;
